6442b752eb54a48d808ae208d24b79f690638ca1
JustimusPrime <hirejustin@yahoo.com>
2018-02-19 15:30:51 -0700

    implemented edit and update

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,6 @@
 class BlogsController < ApplicationController
+    # before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+
   def index
     @blog_posts = Blog.all
   end
@@ -12,19 +14,38 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog_post.save
-        format.html { redirect_to @blog_post, notice: 'Nice blog dude, it is now live' }
+        format.html { redirect_to blogs_path, notice: 'Nice blog upate dude, it is now live' }
       else
         format.html { render :new }
       end
     end
   end
 
+  def edit
+    @blog_post = Blog.find(params[:id])
+  end
+
+  def update
+    @blog_post = Blog.find(params[:id])
+    respond_to do |format|
+      if @blog_post.update(blog_params)
+        format.html { redirect_to blogs_path, notice: 'Nice blog update dude, your changes have been saved'}
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
 
   private
+
+    # def set_blog
+    #   @blog_post = Blog.find(params[:id])
+    # end
+
     def blog_params
       params.require(:blog).permit(:title, :body)
     end
 
-  end
-
 end
+
