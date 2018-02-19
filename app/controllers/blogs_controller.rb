5292f2e64faaed7394cb0f5f9b15b1b76c7c4464
JustimusPrime <hirejustin@yahoo.com>
2018-02-19 14:30:26 -0700

    added create

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,4 +2,29 @@ class BlogsController < ApplicationController
   def index
     @blog_posts = Blog.all
   end
+
+  def new
+    @blog_post = Blog.new
+  end
+
+  def create 
+    @blog_post = Blog.new(blog_params)
+
+    respond_to do |format|
+      if @blog_post.save
+        format.html { redirect_to @blog_post, notice: 'Nice blog dude, it is now live' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
+  private
+    def blog_params
+      params.require(:blog).permit(:title, :body)
+    end
+
+  end
+
 end
