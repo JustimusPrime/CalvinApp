ae6daeae9632da19fa3a8fe152389709b2f2a511
JustimusPrime <hirejustin@yahoo.com>
2018-02-19 23:26:38 -0700

    finished CRUD for show calendar

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -20,6 +20,29 @@ class GigsController < ApplicationController
     end
   end
 
+  def edit
+    @gig = Gig.find(params[:id])
+  end
+
+  def update
+    @gig = Gig.find(params[:id])
+    respond_to do |format|
+      if @gig.update(gig_params)
+        format.html { redirect_to gigs_path, notice: 'You have edited this show, hopefully it`s right this time!'}
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @gig = Gig.find(params[:id])
+    @gig.destroy
+    respond_to do |format|
+      format.html { redirect_to gigs_url, notice: "You have cancelled on this show, which was probably wise" }
+    end
+  end
+
   private
 
   # def set_blog
